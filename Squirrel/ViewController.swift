6494a02c8a5b2d485724c5443de1f4af0543b768
Hannah Jiang <jiangbz@bc.edu>
2020-04-24 01:53:29 -0700

    Initial Commit

---
+++ Squirrel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Squirrel
+//
+//  Created by Hannah Jiang on 4/24/20.
+//  Copyright © 2020 Hannah Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
